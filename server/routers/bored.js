15e7a9b63229cd1437102f7d7d6149ef93d00d2e
gabriellahartanto <gabriella.hartanto@icloud.com>
2020-09-03 20:39:22 +0700

    put bored router to main router

--- server/routers/bored.js
+++ server/routers/bored.js
@@ -5,7 +5,7 @@ const authentication = require('../middlewares/authentication');
 const boredController = require('../controllers/boredController');
 
 router.use(authentication);
-router.get('/bored', boredController.randomActivity);
-router.get('/bored/:type', boredController.chooseActivity);
+router.get('/', boredController.randomActivity);
+router.get('/:type', boredController.chooseActivity);
 
 module.exports = router;
--- server/routers/index.js
+++ server/routers/index.js
@@ -1,6 +1,10 @@
 const route = require('express').Router()
 const userController = require('../controllers/userController')
 
+const routerBoredApi = require('../routers/bored');
+
+route.use('/bored', routerBoredApi);
+
 route.post('/register', userController.register)
 route.post('/login', userController.login)
 
