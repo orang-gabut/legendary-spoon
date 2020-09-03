9a76268e305ba3a0447a97c2d945e41351ece32e
ediyasaedi <ediyasaedi@gmail.com>
2020-09-03 18:55:03 +0700

    -Add register and login logic with hash and jwt token -Add error handler :wq :qw

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,62 @@
 //user controller here
+const { User } = require('../models')
+const { comparePassword } = require('../helpers/bcrypt')
+const { generateToken } = require('../helpers/jwt')
+
+class UserController {
+
+    static async login(req, res, next){
+        try {
+            const { email, password } = req.body
+
+            let user = await User.findOne({
+                where: {
+                    email
+                }
+            })
+
+            if(!user) throw { msg: "invalid email or password"}
+            let comparePass = comparePassword(password, user.password)
+            if(!comparePass) throw { msg: "invalid email or password"}
+            
+            let payload = {
+                id: user.id,
+                email: user.email
+            }
+
+            let token = generateToken(payload)
+            res.status(200).json({ token })
+
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    static async register(req, res, next){
+        try {
+            const { email, password } = req.body
+
+            let data = await User.create({
+                email, password
+            })
+
+            res.status(201).json({
+                id: data.id,
+                email: data.email
+             })
+
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    static async googleSign(req, res, next){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
+
+module.exports = UserController
--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -1,1 +1,12 @@
 //jwt here
+const jwt = require('jsonwebtoken')
+
+function generateToken(payload){
+    return jwt.sign(payload, process.env.SECRET)
+}
+
+function verifyToken(token){
+    return jwt.verify(token, process.env.SECRET)
+}
+
+module.exports = { generateToken, verifyToken }
