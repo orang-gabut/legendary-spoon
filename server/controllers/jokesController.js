e7a901d2bc5a2f4f6ebe31fb72bfe45e3a3e50b1
Bramah Ing Sona <ingsona06@gmail.com>
2020-09-04 05:40:01 +0700

    -fix jokes.js -fix jokesController.js

--- server/controllers/jokesController.js
+++ server/controllers/jokesController.js
@@ -1,16 +1,18 @@
 const axios = require('axios')
 
 class JokesController {
-    static async getJoke(req,res,next) {
-        try {
-          const response = await axios.get('https://jokes.p.rapidapi.com/jod?category=%3Crequired%3E');
-          console.log(response);
-          res.status(200).json({data:response.data})
-        } catch (err) {
-          console.error(err);
-          next(err)
-        }
-      }
+    static getJoke(req,res,next) {
+      
+axios.get('https://official-joke-api.appspot.com/random_joke')
+    .then(response=>{
+      console.log(response)
+      res.status(200).json(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+      next(error)
+    })
+    }
 }
 
 module.exports = JokesController
--- server/routers/jokes.js
+++ server/routers/jokes.js
@@ -4,8 +4,8 @@ const authentication = require('../middlewares/authentication');
 
 const JokesController = require('../controllers/jokesController');
 
-// router.use(authentication);
+router.use(authentication);
 router.get('/', JokesController.getJoke);
-// router.get('/:type', JokesController.chooseActivity);
+
 
 module.exports = router;
